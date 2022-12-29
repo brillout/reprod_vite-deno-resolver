2004d9442c7d98650195b9154edf81de59eb709f
Romuald Brillout <git@brillout.com>
2022-12-29 20:01:39 +0100

    wip

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,8 +3,8 @@ import react from 'npm:@vitejs/plugin-react@^3.0.0'
 import ssr from 'npm:vite-plugin-ssr/plugin'
 // import denoResolve from 'https://deno.land/x/vite_plugin_deno_resolve/mod.ts';
 
-import 'npm:react@^18.2.0'
-import 'npm:react-dom@^18.2.0/client'
+// import 'npm:react@^18.2.0'
+// import 'npm:react-dom@^18.2.0/client'
 
 export default defineConfig({
   plugins: [react(), ssr()
