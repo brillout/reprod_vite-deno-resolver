106f49b58df2b9c45a935b2c5855821b54159770
Romuald Brillout <git@brillout.com>
2022-12-30 19:16:51 +0100

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import ssr from "vite-plugin-ssr/plugin";
-import denoResolve from "vite_plugin_deno_resolve";
+import { defineConfig } from 'npm:vite@^4.0.0'
+import react from 'npm:@vitejs/plugin-react@^3.0.0'
+import ssr from 'npm:vite-plugin-ssr@^0.4.69/plugin'
+import denoResolve from 'https://deno.land/x/vite_plugin_deno_resolve/mod.ts'
+
+import 'npm:react@^18.2.0'
+import 'npm:react-dom@^18.2.0/client'
 
 export default defineConfig({
   plugins: [denoResolve(), react(), ssr()],
